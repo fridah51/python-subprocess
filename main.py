88e5af2407bb84d3e0c000c87af708af8acc8210
fridah51 <fridahshiku1@gmail.com>
2025-04-24 20:58:06 +0300

    kiutyret

--- main.py
+++ main.py
@@ -5,11 +5,9 @@ app = Flask(__name__)
 
 @app.route('/hello')
 def hello():
-    print('heloo')
+
+    return "hello you!"
 
 
-    return
-
-
-
-app.run()
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
