5b98272e3a37b73934410725ccd7e6db1076b13d
fridah51 <fridahshiku1@gmail.com>
2025-04-29 17:09:52 +0300

    echo

--- main.py
+++ main.py
@@ -6,33 +6,42 @@ app = Flask(__name__)
 
 @app.route('/hello')
 def hello():
-    event = "deploy_script.sh flask sasa 5002 fridah51"
+    # event = "deploy_script.sh flask sasa 5002 fridah51"
+    event = "sasa"
+
     script_path = '/app/scripts/hello.sh'
 
     # 1. Make it executable
     subprocess.run(['chmod', '+x', script_path], check=True)
 
     # 2. Run the script and pass the event via stdin
-    result = subprocess.run(['bash', script_path], capture_output=True, text=True)
-    print("STDOUT:", result.stdout)
-    print("STDERR:", result.stderr)
+
+    # OPTION 1
+    # result = subprocess.run(['bash', script_path], capture_output=True, text=True)
+    # print("STDOUT:", result.stdout)
+    # print("STDERR:", result.stderr)
+
+    # return "hello you!"
 
 
-    # process = subprocess.Popen(
-    #     ['bash',script_path],              # path to script (you already chmod’d it)
-    #     stdin=subprocess.PIPE,
-    #     stdout=subprocess.PIPE,
-    #     stderr=subprocess.PIPE,
-    #     text=True                   # for string I/O instead of bytes
-    # )
+    # OPTION 2
+    process = subprocess.Popen(
+        ['bash',script_path],              # path to script (you already chmod’d it)
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True                          # for string I/O instead of bytes
+    )
 
+    stdout, stderr = process.communicate(input=event)
+    
     # stdout, stderr = process.communicate()
 
-    # print("STDOUT:", stdout)
-    # print("STDERR:", stderr)
-    # print("Exit code:", process.returncode)
-
-    return "hello you!"
+    return {
+            "status": "success" if process.returncode == 0 else "error",
+            "stdout": stdout,
+            "stderr": stderr
+        }
 
 
 if __name__ == '__main__':
--- scripts/hello.sh
+++ scripts/hello.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-echo "Hello, World!" > /app/scripts/hello.txt
+read subdomain
 
+echo "Hello, World!" > /app/scripts/hello.txt
+echo "Deploy App Event detected: $subdomain"
 echo "----------------------Running from sh file--------------"
